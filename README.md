bb9df0c2acda86e28eeb4d16c43e42b801a39c36
ryoko.n <nozaki.ryoko@no1s.biz>
2018-05-15 23:50:48 +0900

    01 TwitterAPI 画像検索

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 2018年度テスト用リポジトリ
 
-外部リポジトリをインストール
-composer require abraham/twitteroauth
-
+【test01】
+01 TwitterAPI 画像検索 
+1.外部リポジトリをインストール
+	composer require abraham/twitteroauth
+2.test2018/test01/downloadを作成
+3.Twitter API関連情報を設定
--- test01/01TwitterAPI.php
+++ test01/01TwitterAPI.php
@@ -1,15 +1,18 @@
 <?php
-define('API_KEY', 'xxx');
-define('API_SECRET', 'xxx');
-define('ACCESS_TOKEN', 'xxx');
-define('ACCESS_SECRET', 'xxx');
-		
+// Twitter API関連情報
+define('CONSUMER_KEY', 'xxxx');
+define('CONSUMER_SECRET', 'xxxx');
+define('ACCESS_TOKEN', 'xxxx');
+define('ACCESS_TOKEN_SECRET', 'xxxx');
+
 //「twitteroauth」読み込み
 require_once '../vendor/autoload.php';
 
+// 開始ログ
+echo "start... \r\n";
+
 //トークンを記述
-//$connection = new \Abraham\TwitterOAuth\TwitterOAuth("Consumer Key (API Key)","Consumer Secret (API Secret)","Access Token","Access Token Secret");
-$connection = new \Abraham\TwitterOAuth\TwitterOAuth(API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_SECRET);
+$connection = new \Abraham\TwitterOAuth\TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET);
 
 //ツイート取得
 /*
@@ -17,17 +20,43 @@ $connection = new \Abraham\TwitterOAuth\TwitterOAuth(API_KEY,API_SECRET,ACCESS_T
  * result_type：取得ツイートの種類[popular,recent,mixed]
  * count：取得ツイート数
  */
-$tweets_params = array('q' => 'JustinBieber','result_type'=>'recent','count' => '10');
+$tweets_params = array('q' => 'JustinBieber','result_type'=>'recent','count' => '100');
 $tweets = $connection->get('search/tweets', $tweets_params)->statuses;
 
+$count = 0;
+$data = [];
+
 foreach ($tweets as $value) {
-	foreach($value->extended_entities->media as $value_media){
-		if($value_media->type == 'photo'){
-			$media_id = $value_media->id;		//画像ID
-			$media_url = $value_media->media_url;		//画像URL
-			echo $media_id.PHP_EOL.$media_url.PHP_EOL;
-// 			$data = file_get_contents($media_url);
-// 			file_put_contents('./download/dl.jpg',$data);PHP_EOL
+
+	// $value->extended_entities->mediaが存在する場合のみ
+	if(isset($value->extended_entities->media)){
+		foreach($value->extended_entities->media as $value_media){
+		
+			// 画像の場合のみ
+			if($value_media->type == 'photo'){
+			
+				// 画像URL
+				$media_url = $value_media->media_url;
+				// 画像名
+				$name = substr($media_url,strripos ($media_url, '/')+1);
+				
+				// 重複する画像は除外
+				if(!in_array($media_url, $data)){
+				
+					// 画像を出力
+					$file = file_get_contents($media_url);
+					file_put_contents('./download/'.$name, $file);
+					$data[] = $media_url;
+
+					// 10枚取得したら終了
+					$count++;
+					if( $count === 10) break 2;
+				}
+			}
 		}
 	}
 }
+
+// 終了ログ
+echo "...end \r\n";
+exit;
